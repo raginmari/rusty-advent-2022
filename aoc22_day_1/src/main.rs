89925f42c9fa4399c3d80f5c9bdd091b165870ef
Reimar Twelker <reimar.twelker@web.de>
2022-12-02 19:01:11 +0100

    main: Add solution for second star.

--- aoc22_day_1/src/main.rs
+++ aoc22_day_1/src/main.rs
@@ -1,4 +1,3 @@
-use std::cmp::max;
 use std::fs::File;
 use std::io::{self, BufRead, Lines};
 use std::path::Path;
@@ -8,18 +7,19 @@ fn main()
     // TIL: file paths are specified relative to the folder where Cargo.toml is located.
     let input_path = "res/input.txt";
     let input = read_lines(input_path).expect("Should find puzzle input");
-    solve_1(input);
+    
+    solve(input, 3);
 }
 
 // TIL: 'traits' are (kind of) Rust's protocols (Swift) or interfaces (C#).
-fn solve_1<T>(input: Lines<T>) where T: BufRead
+fn solve<T>(input: Lines<T>, top: usize) where T: BufRead
 {
-    let mut max_calories = 0;
+    let mut calories_per_elf = Vec::new();
     let mut acc = 0;
     for maybe_line in input {
         let line = maybe_line.unwrap();
         if line.is_empty() {
-            max_calories = max(max_calories, acc);
+            calories_per_elf.push(acc);
             acc = 0;
         } else {
             // TIL: use 'turbofish' ::<i32> or the explicit type i32 to tell the compiler what to parse / the generic type.
@@ -29,9 +29,12 @@ fn solve_1<T>(input: Lines<T>) where T: BufRead
     }
 
     // Don't forget the last elf
-    max_calories = max(max_calories, acc);
-    
-    println!("The elf carrying the most calories has {} calories", max_calories);
+    calories_per_elf.push(acc);
+
+    calories_per_elf.sort_unstable();
+    let total_calories = calories_per_elf.iter().rev().take(top).sum::<i32>();
+
+    println!("The top {0} elves carry {1} calories", top, total_calories);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>
