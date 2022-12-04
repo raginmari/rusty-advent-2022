a8b76c6ff2134ade599028b05cd3767ddd2bc172
Reimar Twelker <reimar.twelker@web.de>
2022-12-04 21:48:35 +0100

    main: Extract tools module to crate.

--- aoc22_day_2/src/main.rs
+++ aoc22_day_2/src/main.rs
@@ -1,3 +1,5 @@
+use tools;
+
 fn main() 
 {
     let input_path = "res/input.txt";
@@ -64,19 +66,6 @@ fn points_for(opponent: &Shape, me: &Shape) -> i32
 
         // Draw
         _ => 3
-    }
-}
-
-mod tools
-{
-    use std::fs::File;
-    use std::io::{self, BufRead};
-    use std::path::Path;
-    
-    pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>
-    {
-        let file = File::open(filename)?;
-        Ok(io::BufReader::new(file).lines())
     }
 }
 
