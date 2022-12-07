875bcd54ec8a4a63bef82cbb5c18ee7c7fca8bed
Reimar Twelker <reimar.twelker@web.de>
2022-12-08 00:40:09 +0100

    main: Add (pretty rubbish?) solution for second star.

--- aoc22_day_3/src/main.rs
+++ aoc22_day_3/src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::HashSet;
-
 use tools;
 
 fn main() {
@@ -8,11 +7,10 @@ fn main() {
         .expect("Should find puzzle input")
         .map(|x| x.unwrap());
     
-        println!("The sum of priorities is {}", solve_star_1(input));
+        println!("The sum of priorities is {}", solve_star_2(input));
 }
 
 #[allow(dead_code)]
-// TIL: requiring IntoIterator makes it possible to call the function with Vec as well as an Iterator
 fn solve_star_1<I>(input: I) -> i32 where I: IntoIterator<Item = String>
 {
     input.into_iter().map(|line| {
@@ -21,6 +19,31 @@ fn solve_star_1<I>(input: I) -> i32 where I: IntoIterator<Item = String>
         let set_b = b.chars().collect::<HashSet<char>>();
         let duplicate = set_a.intersection(&set_b).next().expect("intersection should not be empty");
         priority_of(duplicate)
+    }).sum()
+}
+
+#[allow(dead_code)]
+fn solve_star_2<I>(input: I) -> i32 where I: IntoIterator<Item = String>
+{
+    let mut backpacks: Vec<String> = Vec::new();
+    input.into_iter().map(|line| {
+        
+        backpacks.push(line);
+
+        if backpacks.len() < 3 {
+            return 0 // Do not add to the sum
+        }
+        
+        let set_a = backpacks[0].chars().collect::<HashSet<char>>();
+        let set_b = backpacks[1].chars().collect::<HashSet<char>>();
+        let set_c = backpacks[2].chars().collect::<HashSet<char>>();
+        backpacks.clear();
+
+        let int_1 = set_a.intersection(&set_b).collect::<HashSet<&char>>();
+        let int_2 = set_a.intersection(&set_c).collect::<HashSet<&char>>();
+        let badge = **int_1.intersection(&int_2).next().expect("intersection should not be empty");
+
+        priority_of(&badge)
     }).sum()
 }
 
@@ -41,7 +64,6 @@ fn priority_of(c: &char) -> i32
 
 mod tests
 {
-    // TIL: Uses names from parent scope including private symbols
     #[allow(unused_imports)]   
     use super::*;
 
@@ -59,5 +81,21 @@ mod tests
         let input = lines.iter().map(|x| x.to_string());
         let expected_sum_of_priorities = solve_star_1(input);
         assert_eq!(expected_sum_of_priorities, 157);
+    }
+
+    #[test]
+    fn test_example_star_2()
+    {
+        let lines = vec![
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+        ];
+        let input = lines.iter().map(|x| x.to_string());
+        let expected_sum_of_priorities = solve_star_2(input);
+        assert_eq!(expected_sum_of_priorities, 70);
     }
 }
