d145d408b490349226695f33e10afc9ae8f7d058
Reimar Twelker <reimar.twelker@web.de>
2022-12-27 01:11:39 +0100

    main: Add solution for second star.
    Refactor.

--- aoc22_day_4/src/main.rs
+++ aoc22_day_4/src/main.rs
@@ -6,10 +6,29 @@ fn main() {
         .expect("Should find puzzle input")
         .map(|x| x.unwrap());
     
-        println!("The result is {}", solve_star_1(input));
+        println!("The result is {}", solve_star_2(input));
 }
 
+struct Segment { left: i32, right: i32 }
+
+#[allow(dead_code)]
 fn solve_star_1<I>(input: I) -> i32 where I: IntoIterator<Item = String>
+{
+    solve_star(input, |s1, s2| {
+        s1.left <= s2.left && s1.right >= s2.right || 
+        s2.left <= s1.left && s2.right >= s1.right
+    })
+}
+
+#[allow(dead_code)]
+fn solve_star_2<I>(input: I) -> i32 where I: IntoIterator<Item = String>
+{
+    solve_star(input, |s1, s2| {
+        s1.right >= s2.left && s1.left <= s2.right
+    })
+}
+
+fn solve_star<I>(input: I, f: fn(Segment, Segment) -> bool) -> i32 where I: IntoIterator<Item = String>
 {
     let mut result = 0;
 
@@ -19,10 +38,10 @@ fn solve_star_1<I>(input: I) -> i32 where I: IntoIterator<Item = String>
         }).flatten().collect::<Vec<_>>();
 
         assert_eq!(camp_sections.len(), 4);
-        let (left_1, left_2, right_1, right_2) = 
-            (camp_sections[0], camp_sections[2], camp_sections[1], camp_sections[3]);
+        let s1 = Segment { left: camp_sections[0], right: camp_sections[1] };
+        let s2 = Segment { left: camp_sections[2], right: camp_sections[3] };
 
-        if (left_1 <= left_2 && right_1 >= right_2) || (left_2 <= left_1 && right_2 >= right_1) {
+        if f(s1, s2) {
             result += 1;
         }
     }
@@ -47,7 +66,23 @@ mod tests
             "2-6,4-8"
         ];
         let input = lines.iter().map(|x| x.to_string());
-        let expected_sum_of_priorities = solve_star_1(input);
-        assert_eq!(expected_sum_of_priorities, 2);
+        let expected_contained_segments = solve_star_1(input);
+        assert_eq!(expected_contained_segments, 2);
+    }
+
+    #[test]
+    fn test_example_star_2()
+    {
+        let lines = vec![
+            "2-4,6-8",
+            "2-3,4-5",
+            "5-7,7-9",
+            "2-8,3-7",
+            "6-6,4-6",
+            "2-6,4-8"
+        ];
+        let input = lines.iter().map(|x| x.to_string());
+        let expected_overlapping_segments = solve_star_2(input);
+        assert_eq!(expected_overlapping_segments, 4);
     }
 }
