02b9ce0bcdab46e2f310d45fe983ed2c98985442
Reimar Twelker <reimar.twelker@web.de>
2022-12-30 09:29:45 +0100

    main: Add project for day 4.
    Add solution for first star.

---
+++ aoc22_day_5/src/main.rs
@@ -0,0 +1,123 @@
+use tools;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+fn main() {
+    let initial_stacks = vec![
+        "SZPDLBFC",
+        "NVGPHWB",
+        "FWBJG",
+        "GJNFLWCS",
+        "WJLTPMSH",
+        "BCWGFS",
+        "HTPMQBW",
+        "FSWT",
+        "NCR"
+    ];
+
+    let input_path = "res/input.txt";
+    let input = tools::read_lines(input_path)
+        .expect("Should find puzzle input")
+        .map(|x| x.unwrap());
+    
+    let ship = create_ship(&initial_stacks);
+    println!("The result is {}", solve_star_1(ship, input));
+}
+
+#[derive(Debug)]
+struct Ship
+{
+    container_stacks: Vec<ContainerStack>
+}
+
+impl Ship {
+    fn top_containers(&self) -> String
+    {
+        self.container_stacks.iter().map(|x| x.containers.last().unwrap_or(&'\0')).collect::<String>()
+    }
+}
+
+#[derive(Debug)]
+struct ContainerStack
+{
+    containers: Vec<char>
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Step
+{
+    count: i32,
+    from_stack: usize,
+    to_stack: usize
+}
+
+#[allow(dead_code)]
+fn solve_star_1<I>(mut ship: Ship, step_strings: I) -> String where I: IntoIterator<Item = String>
+{
+    let steps = step_strings.into_iter().map(|x| create_step(&x));
+
+    for step in steps {
+        for _ in 0..step.count {
+            let to_move = ship.container_stacks[step.from_stack].containers.pop().unwrap();
+            ship.container_stacks[step.to_stack].containers.push(to_move)
+        }
+    }
+
+    ship.top_containers()
+}
+
+fn create_ship(stacks: &Vec<&str>) -> Ship
+{
+    let mut ship = Ship { container_stacks: Vec::new() };
+
+    for stack in stacks {
+        let containers = stack.chars().collect::<Vec<_>>();
+        let container_stack = ContainerStack { containers };
+        ship.container_stacks.push(container_stack);
+    }
+
+    ship
+}
+
+fn create_step(step_string: &str) -> Step
+{
+    lazy_static! {
+        static ref STEP_REGEX: Regex = Regex::new("move (\\d+) from (\\d) to (\\d)").unwrap();
+    }
+
+    let captures = STEP_REGEX.captures(step_string).unwrap();
+    assert_eq!(captures.len(), 1 + 3, "missing captures");
+    
+    Step {
+        count: captures.get(1).unwrap().as_str().parse::<i32>().unwrap(),
+        from_stack: captures.get(2).unwrap().as_str().parse::<usize>().unwrap() - 1,
+        to_stack: captures.get(3).unwrap().as_str().parse::<usize>().unwrap() - 1
+    }
+}
+
+mod tests
+{
+    #[allow(unused_imports)]   
+    use super::*;
+
+    #[test]
+    fn test_example_star_1()
+    {
+        let stacks = vec![
+            "ZN",
+            "MCD",
+            "P"
+        ];
+
+        let steps = vec![
+            String::from("move 1 from 2 to 1"),
+            String::from("move 3 from 1 to 3"),
+            String::from("move 2 from 2 to 1"),
+            String::from("move 1 from 1 to 2")
+        ];
+        
+        let ship = create_ship(&stacks);
+        let expected_top_containers = solve_star_1(ship, steps);
+        assert_eq!(expected_top_containers, "CMZ");
+    }
+}
